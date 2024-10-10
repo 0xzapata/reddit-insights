a91045bbe583be571398bd47aeee1007d5833a3a
Niel <niel@migokartel.xyz>
2024-10-10 14:27:04 +0800

    feat: added the proper env database entry

--- src/env.mjs
+++ src/env.mjs
@@ -4,7 +4,7 @@ import { vercel } from "@t3-oss/env-nextjs/presets";
 
 export const env = createEnv({
   server: {
-    DATABASE_URL: z.string().url(),
+    TURSO_CONNECTION_URL: z.string().url(),
     TURSO_AUTH_TOKEN: z.string().min(1),
     CLERK_SECRET_KEY: z.string().min(1),
   },
